1d35b2684b837ddaec520acbf0f422a8be321d0b
nagiyoru <xml001001@126.com>
2024-06-12 11:42:02 +0800

    feat(utils): 通过Jsoup解析iframe src并改进WebHook发送逻辑
    - 引入Jsoup库用于解析iframe标签中的src属性。- 在MailFactory中使用Jsoup解析iframe src，以获取登录链接。
    - 优化WebHookUtils中消息发送的处理逻辑，增加发送失败的警告日志。
    - 添加对webhookToken的配置项，改进webhook地址的构造方式。

--- src/main/java/com/xmlin/wechatter/wechatbot/utils/WebHookUtils.java
+++ src/main/java/com/xmlin/wechatter/wechatbot/utils/WebHookUtils.java
@@ -2,6 +2,8 @@ package com.xmlin.wechatter.wechatbot.utils;
 
 import cn.hutool.core.lang.Assert;
 import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.xmlin.wechatter.wechatbot.entity.SendMsgBody;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +16,8 @@ public class WebHookUtils
 
     @Value("${wechatter.webhookUrl}")
     public String webhookUrl;
+    @Value("${wechatter.webhookToken}")
+    public String token;
 
     /**
      * 发送微信消息
@@ -23,8 +27,15 @@ public class WebHookUtils
      */
     public void sendMsg(String to, String content) {
         Assert.notBlank(to, "发送人不能为空");
-        log.info("send---<{}>:<{}>", to, content);
-        HttpUtil.post(webhookUrl, new SendMsgBody(to, content).build().toJSONString());
+        String toUseUrl = webhookUrl + "/webhook/msg/v2?token=" + token;
+        String post = HttpUtil.post(toUseUrl, new SendMsgBody(to, content).build().toJSONString());
+        JSONObject jsonObject = JSON.parseObject(post);
+        if (!jsonObject.getBooleanValue("success")) {
+            log.warn("消息发送失败：{} <{}>:<{}>", jsonObject.getString("message"), to, content);
+        }
+        else {
+            log.info("send---<{}>:<{}>", to, content);
+        }
     }
 
 }
